2101b6904e17520ff22419926839b95cccc8706e
unknown <2003dileepmeena@gmail.com>
2024-05-21 19:43:33 +0530

    new file create

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const app=express();
+const path=require("path");
+
 
 main().then(()=>{
     console.log("Connected to MongoDB");
@@ -10,9 +12,18 @@ main().then(()=>{
 
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"/public"))); 
+
+app.get("/",(req,res)=>{
+    res.send("welcome to first route");
+})
 
     app.listen(8080,(req,res)=>{
         console.log("listing port");
     })
 
-}
+
